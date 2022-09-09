0729f11efbfa4a3ae05e593d9d67001cd3f5464b
drt <83830909+drtovski@users.noreply.github.com>
2022-09-09 14:46:43 +0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
 module.exports = {
-    token: process.env.TOKEN || "",  // your bot token
-    clientID: process.env.CLIENT_ID || "", // your bot client id
+    token: process.env.TOKEN || "MTAxNzcxNzAzMjI2OTExOTUyOA.GNuvgD.zG_bFBoDfA3e4SPXV4-7I55EwvJ5ZM5nYGusB0",  // your bot token
+    clientID: process.env.CLIENT_ID || "1017717032269119528", // your bot client id
     prefix: process.env.PREFIX || "!", // bot prefix
-    ownerID: process.env.OWNERID || "", //your discord id
+    ownerID: process.env.OWNERID || "1017717032269119528", //your discord id
     SpotifyID: process.env.SPOTIFYID || "",
     SpotifySecret: process.env.SPOTIFYSECRET || "",
     mongourl: process.env.MONGO_URI || "", // MongoDb URL
@@ -17,10 +17,10 @@ module.exports = {
     },
     nodes: [
         {
-            host: process.env.NODE_HOST || "54.37.6.86",
-            port: parseInt(process.env.NODE_PORT || "80"),
-            password: process.env.NODE_PASSWORD || "Blacky#9125",
-            secure: parseBoolean(process.env.NODE_SECURE || "false"),
+            host: process.env.NODE_HOST || "lavalink.oops.wtf",
+            port: parseInt(process.env.NODE_PORT || "443"),
+            password: process.env.NODE_PASSWORD || "www.freelavalink.ga",
+            secure: parseBoolean(process.env.NODE_SECURE || "true"),
 
         }
     ],
