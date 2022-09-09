321be9172e7f47925d3e67e1a66cc2a502ade665
drt <83830909+drtovski@users.noreply.github.com>
2022-09-09 14:55:19 +0600

    Update interactionCreate.js

--- src/events/Client/interactionCreate.js
+++ src/events/Client/interactionCreate.js
@@ -1,7 +1,4 @@
 const { CommandInteraction, Client, InteractionType, PermissionFlagsBits, EmbedBuilder } = require("discord.js")
-const db = require("../../schema/prefix.js");
-const db2 = require("../../schema/dj");
-const db3 = require("../../schema/setup");
 
 module.exports = {
     name: "interactionCreate",
@@ -11,10 +8,7 @@ module.exports = {
  * @param {CommandInteraction} interaction 
  */
     run: async (client, interaction) => {
-        let prefix = client.prefix;
-        const ress = await db.findOne({ Guild: interaction.guildId })
-        if (ress && ress.Prefix) prefix = ress.Prefix;
-
+        
         if (interaction.type === InteractionType.ApplicationCommand) {
             const command = client.slashCommands.get(interaction.commandName);
             if (!command) return;
@@ -67,22 +61,7 @@ module.exports = {
                     }
                 }
             }
-            if (command.dj) {
-                let data = await db2.findOne({ Guild: interaction.guildId })
-                let perm = PermissionFlagsBits.MuteMembers;
-                if (data) {
-                    if (data.Mode) {
-                        let pass = false;
-                        if (data.Roles.length > 0) {
-                            interaction.member.roles.cache.forEach((x) => {
-                                let role = data.Roles.find((r) => r === x.id);
-                                if (role) pass = true;
-                            });
-                        };
-                        if (!pass && !interaction.member.permissions.has(perm)) return await interaction.reply({ content: `You don't have permission or dj role to use this command`, ephemeral: true })
-                    };
-                };
-            };
+            
 
             try {
                 await command.run(client, interaction, prefix);
@@ -102,9 +81,6 @@ module.exports = {
             
         } 
 
-        if (interaction.isButton()) {
-            let data = await db3.findOne({ Guild: interaction.guildId });
-            if (data && interaction.channelId === data.Channel && interaction.message.id === data.Message) return client.emit("playerButtons", interaction, data);
-        };
+        
     }
 };
